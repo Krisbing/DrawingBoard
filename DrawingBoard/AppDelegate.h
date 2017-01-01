df060708b4714c52de70ce4d5fbecab8db39ded1
songjie <jw007800@yeah.net>
2017-01-01 13:08:18 +0800

    Initial Commit

---
+++ DrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingBoard
+//
+//  Created by songjie on 2017/1/1.
+//  Copyright © 2017年 songjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingBoard
+//
+//  Created by songjie on 2017/1/1.
+//  Copyright © 2017年 songjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
